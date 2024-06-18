b19e9c8eeb59e84213bd87933d08a3a9c7d73e6b
Stephaniechan2005 <stephanie_chan2005@yahoo.com.hk>
2024-06-18 15:05:58 -0400

    Update the burger menu and footer
    In this commit the burger menu and footer updated.

--- js/main.js
+++ js/main.js
@@ -1,40 +1,14 @@
-console.log("JS file connected");
+(function () {
+  "use strict";
+  console.log("fired");
 
-let temperature = 31;
-//basic if statement
-if (temperature > 30) {
-  console.log("Time to crank the AC!");
-}
+  let burger = document.querySelector("#button");
+  let burgerCon = document.querySelector("#burger-con");
 
-// if else statement
-let age = 20;
+  function hamburgerMenu() {
+    burger.classList.toggle("expanded");
+    burgerCon.classList.toggle("slide-toggle");
+  }
 
-if (age < 19) {
-  console.log("Sorry, you are not old enough to enter bar");
-} else {
-  console.log("You are 19 or older, so come on in");
-}
-
-let grade = 75;
-if (grade >= 90) {
-  console.log("A+");
-} else if (grade >= 80) {
-  console.log("A");
-} else if (grade >= 70) {
-  console.log("B");
-} else {
-  console.log("Failing Grade");
-}
-
-//multiple if statements (can all evaluate to true)
-
-let mathGrade = 90;
-if (mathGrade >= 90) {
-  console.log("Math grade is A+");
-}
-if (mathGrade >= 80) {
-  console.log("Math grade is A");
-}
-if (mathGrade >= 70) {
-  console.log("Math grade is B");
-}
+  burger.addEventListener("click", hamburgerMenu, false);
+})();
